760d2cbcaec6a6e927262e6d94eed1abffa28a56
prussian1to9 <prussian.3to3@gmail.com>
2025-02-06 14:09:49 +0900

    [INIT] 0.0.0 proj. settings

---
+++ webpack.config.js
@@ -0,0 +1,65 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const InterpolateHtmlPlugin = require("interpolate-html-plugin");
+
+const path = require("path");
+
+module.exports = {
+  mode: "development" /** @FIXME when production */,
+  entry: "./src/index.tsx",
+  devtool: "inline-source-map",
+  output: {
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "dist"),
+    publicPath: "/",
+  },
+  resolve: {
+    extensions: [".ts", ".tsx", ".js"],
+    alias: {
+      "@types": path.resolve(__dirname, "types/"),
+      "@api": path.resolve(__dirname, "src/api/"),
+      "@utils": path.resolve(__dirname, "src/utils/"),
+      "@components": path.resolve(__dirname, "src/components/"),
+      "@pages": path.resolve(__dirname, "src/pages/"),
+      "@styles": path.resolve(__dirname, "src/styles/"),
+      "@assets": path.resolve(__dirname, "src/assets/"),
+      Types: path.resolve(__dirname, "src/types/"),
+    },
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: "ts-loader",
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.svg$/,
+        use: ["@svgr/webpack", "url-loader"],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./public/index.html",
+    }),
+    new InterpolateHtmlPlugin({
+      /** @FIXME when use HTML biases */
+    }),
+  ],
+  devServer: {
+    static: [
+      { directory: path.join(__dirname, "dist") },
+      { directory: path.join(__dirname, "public") },
+    ],
+    historyApiFallback: true,
+    compress: true,
+    port: 8080,
+  },
+  watchOptions: {
+    ignored: "/node_modules/",
+  },
+};
